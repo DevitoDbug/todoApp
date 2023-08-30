5f46e884dd88cbde3977493f0771f332350245a7
DevitoDbug <Davidochiengy@gmail.com>
2023-08-30 04:20:53 +0300

    Creating connection with the db

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+var db *gorm.DB
+
+func ConnectToDB() {
+	DBConnectionString := "root:davi@tcp(localhost:3306)/bookstore?charset=utf8&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", DBConnectionString)
+	if err != nil {
+		log.Printf("Could not connet to the DB:\n%v\n", err)
+		return
+	}
+	db = d
+}
+
+func getDBConnection() *gorm.DB {
+	return db
+}
