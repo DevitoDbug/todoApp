d04d4c00f3454c7387130efc381be9e440af20b0
DevitoDbug <Davidochiengy@gmail.com>
2023-08-30 04:58:21 +0300

    Implemented the getAllTasks handler function

--- pkg/controllers/taskListControler.go
+++ pkg/controllers/taskListControler.go
@@ -1,9 +1,28 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/DevitoDbug/todoApp/pkg/models"
+	"log"
+	"net/http"
+)
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	tasks := models.GetAllTasks()
 
+	res, err := json.Marshal(*tasks)
+	if err != nil {
+		log.Printf("Could not marshal tasks to json:\n%v\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(res)
+	if err != nil {
+		log.Printf("Writting to header error:\n%v\n", err)
+		return
+	}
 }
 func GetTask(w http.ResponseWriter, r *http.Request) {
 
