34e468a8a2b7ced586772227e6d4729e27738557
DevitoDbug <Davidochiengy@gmail.com>
2023-08-30 06:14:24 +0300

    Implemented the updateTask handler

--- pkg/controllers/taskListControler.go
+++ pkg/controllers/taskListControler.go
@@ -91,5 +91,42 @@ func DeleteTask(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	newTask := &models.Task{}
+	ID := utils.IDFromRouteVariable(r)
+	utils.ParseBody(r, newTask)
 
+	updateTask, dbInfo := models.GetTaskBYID(ID)
+	if dbInfo.Error != nil {
+		log.Printf("Getting data from DB error:\n%v\n", dbInfo.Error)
+		return
+	}
+	if newTask.Name != "" {
+		updateTask.Name = newTask.Name
+	}
+	if newTask.Status != "" {
+		updateTask.Status = newTask.Status
+	}
+	if newTask.Priority != "" {
+		updateTask.Priority = newTask.Priority
+	}
+	if newTask.Notes != "" {
+		updateTask.Notes = newTask.Notes
+	}
+
+	//updating the db
+	dbInfo.Save(&updateTask)
+
+	res, err := json.Marshal(updateTask)
+	if err != nil {
+		log.Printf("Could not marshal tasks to json:\n%v\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(res)
+	if err != nil {
+		log.Printf("Writting to header error:\n%v\n", err)
+		return
+	}
 }
